a877efee7e7ea309a2a5d9f161a1f4f0acdf36f2
rohitmehlawat <rohitmehlawat27@gmail.com>
2018-08-28 12:59:29 +0530

    First commit of the project

---
+++ src/main/java/com/packt/webstore/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.packt.webstore.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String welcome(Model model) {
+		
+		model.addAttribute("greeting", "welcome to web store");
+		model.addAttribute("tagline", "The one and only one amzing web store");
+		return "welcome";
+	}
+	
+}
